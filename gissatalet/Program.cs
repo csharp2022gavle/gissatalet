e7b25d486190b111f5608967c49d6a090d68188b
lipadeus <tony.hultqvist@gmail.com>
2022-09-06 04:30:01 -0700

    Intro upload

--- gissatalet/Program.cs
+++ gissatalet/Program.cs
@@ -2,9 +2,52 @@
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool startaSpel = true;
+            /* Titel sida
+             * New game
+                Startar ett nytt spel direkt med en ny skärm
+                    Gissa ett tal mellasn 1 - 10
+                    Gissar du rätt skriv in ditt namn
+                    Fortsätt spela eller avsluta
+             * Highscore
+                - Namn, Poäng(antal rätt gissningar)
+
+             * Quit
+            */
+            while (startaSpel = true) 
+            {
+                string userValue = Console.ReadLine();
+                Meny(startaSpel);
+                if (userValue == "1")
+                {
+                    NewGame();
+                }
+                if (userValue == "2")
+                {
+                    Highscore();
+                }
+                if (userValue == "3") 
+                {
+                    startaSpel = false;
+                }
+            }
+
+        }
+        public static void Meny(Boolean gameOn)
+        {
+            
+
+        }
+        public static void NewGame() 
+        {
+        
+        }
+        public static void Highscore() 
+        {
+        
         }
     }
 }
