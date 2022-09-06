cab9f639e1ce1d309dab0345c565fa8216259eff
lipadeus <tony.hultqvist@gmail.com>
2022-09-06 05:20:50 -0700

    newgame

--- gissatalet/Program.cs
+++ gissatalet/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace gissatalet
+﻿using System.Collections.Generic;
+using System.Reflection.Metadata;
+
+namespace gissatalet
 {
     internal class Program
     {
@@ -6,7 +9,7 @@
         static void Main(string[] args)
         {
             bool startaSpel = true;
-            /* Titel sida
+            /* Titel sida , meny, promt 1-3
              * New game
                 Startar ett nytt spel direkt med en ny skärm
                     Gissa ett tal mellasn 1 - 10
@@ -33,9 +36,13 @@
                 {
                     startaSpel = false;
                 }
+                Console.ReadLine();
             }
 
         }
+        public static List<string> userList = new List<string>();
+        public static List<int> userScore = new List<int>();
+        public static int tempUserScore;
         public static void Meny(Boolean gameOn)
         {
             
@@ -43,11 +50,27 @@
         }
         public static void NewGame() 
         {
-        
+            Console.WriteLine("Nu startas ett spel skriv ditt namn:");
+            string name = Console.ReadLine();
+            userList.Add(name);
+            userScore.Add(0);
+            foreach (var user in userList)
+            {
+                if (userList.Contains(user)) 
+                {
+                    int tempUserScore = userList.IndexOf(user);
+                    string userBack = string.Format("Du {0} har {1} poäng!", user, tempUserScore);
+                    Console.WriteLine(userBack);
+                }
+            }
+
+            for (int i = 0; i < userScore.Count; i++)
+            {
+                            }
         }
         public static void Highscore() 
         {
-        
+            
         }
     }
 }
