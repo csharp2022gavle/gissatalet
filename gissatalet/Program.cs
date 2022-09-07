d90d0afaacb1c653413541d60095aa19c5f61371
Lipa <tony.hultqvist@gmail.com>
2022-09-07 14:20:42 +0200

    Full program, missing line length optimisation.

--- gissatalet/Program.cs
+++ gissatalet/Program.cs
@@ -1,96 +1,261 @@
-﻿using System.Collections.Generic;
-using System.Reflection.Metadata;
-
-namespace gissatalet
+﻿namespace gissatalet
 {
     internal class Program
     {
-
+        public static List<string> userList = new List<string>();
+        public static List<int> userScore = new List<int>();
+        public static int tempUserScore;
+        public static int Xpos = 13;
+        public static void Init() { Xpos = 13; Console.Clear(); }
+        public static WindowWidth windowWidth = new WindowWidth();
         static void Main(string[] args)
         {
             bool startaSpel = true;
-            /* Titel sida , meny, promt 1-3
-             * New game
-                Startar ett nytt spel direkt med en ny skärm
-                    Gissa ett tal mellasn 1 - 10
-                    Gissar du rätt skriv in ditt namn
-                    Fortsätt spela eller avsluta
-             * Highscore
-                - Namn, Poäng(antal rätt gissningar)
-
-             * Quit
-            */
-            while (startaSpel = true)
+            while (startaSpel == true)
             {
                 Titel();
+                Meny();
+                int xPos = 20;
+                int windiwSize = 60;
+                string makeAMove = "Gör ett val: ";
+                Console.SetCursorPosition(windiwSize - makeAMove.Length/2, xPos);
+                Console.Write(makeAMove);
                 string userValue = Console.ReadLine();
-                Meny(startaSpel);
                 if (userValue == "1")
                 {
+                    Init();
                     NewGame();
                 }
                 if (userValue == "2")
                 {
+                    Init();
                     Highscore();
                 }
                 if (userValue == "3")
                 {
                     startaSpel = false;
+                    Console.Clear();
                 }
-                Console.ReadLine();
             }
 
         }
-        public static List<string> userList = new List<string>();
-        public static List<int> userScore = new List<int>();
-        public static int tempUserScore;
-        public static void Meny(Boolean gameOn)
+        public static void Meny()
         {
-            
-
+            Xpos = 13;
+            Console.ForegroundColor = ConsoleColor.White;
+            string optionOne = string.Format("1) Spela Gissa Talet!");
+            string optionTwo = string.Format("2) Se Highscore!");
+            string optionThree = string.Format("3) Avsluta :(");
+            Console.SetCursorPosition(windowWidth.SetWidth(optionOne), windowWidth.SetXpos(1));
+            Console.Write(optionOne);
+            Console.SetCursorPosition(windowWidth.SetWidth(optionTwo), windowWidth.SetXpos(2));
+            Console.Write(optionTwo);
+            Console.SetCursorPosition(windowWidth.SetWidth(optionThree), windowWidth.SetXpos(3));
+            Console.Write(optionThree);
         }
         public static void NewGame()
         {
-            Console.WriteLine("Nu startas ett spel skriv ditt namn:");
+            string title = @" 
+                         ███▄    █ ▓█████  █     █░     ▄████  ▄▄▄       ███▄ ▄███▓▓█████  ▐██▌ 
+                         ██ ▀█   █ ▓█   ▀ ▓█░ █ ░█░    ██▒ ▀█▒▒████▄    ▓██▒▀█▀ ██▒▓█   ▀  ▐██▌ 
+                        ▓██  ▀█ ██▒▒███   ▒█░ █ ░█    ▒██░▄▄▄░▒██  ▀█▄  ▓██    ▓██░▒███    ▐██▌ 
+                        ▓██▒  ▐▌██▒▒▓█  ▄ ░█░ █ ░█    ░▓█  ██▓░██▄▄▄▄██ ▒██    ▒██ ▒▓█  ▄  ▓██▒ 
+                        ▒██░   ▓██░░▒████▒░░██▒██▓    ░▒▓███▀▒ ▓█   ▓██▒▒██▒   ░██▒░▒████▒ ▒▄▄  
+                        ░ ▒░   ▒ ▒ ░░ ▒░ ░░ ▓░▒ ▒      ░▒   ▒  ▒▒   ▓▒█░░ ▒░   ░  ░░░ ▒░ ░ ░▀▀▒ 
+                        ░ ░░   ░ ▒░ ░ ░  ░  ▒ ░ ░       ░   ░   ▒   ▒▒ ░░  ░      ░ ░ ░  ░ ░  ░ 
+                           ░   ░ ░    ░     ░   ░     ░ ░   ░   ░   ▒   ░      ░      ░       ░ 
+                                 ░    ░  ░    ░             ░       ░  ░       ░      ░  ░ ░    ";
+            Console.ForegroundColor = ConsoleColor.DarkRed; Console.WriteLine(title);
+            Console.ForegroundColor = ConsoleColor.White;
+            bool nyttSpel = true;
+            Random slump = new Random();
+            string input = "Nu startas ett spel skriv ditt namn";
+            Console.SetCursorPosition(windowWidth.SetWidth(input), Xpos);
+            Console.Write(input);
+            Console.SetCursorPosition(windowWidth.MaxWidth(), Xpos+1);
+            Console.Write("> ");
             string name = Console.ReadLine();
-            userList.Add(name);
-            userScore.Add(0);
-            foreach (var user in userList)
+            Console.SetCursorPosition(windowWidth.SetWidth(input), Xpos);
+            Console.Write("                                                 ");
+
+            userList.Add("Tony");
+            userScore.Add(3);
+            while (nyttSpel == true)
             {
-                if (userList.Contains(user))
+                int slumpTal = slump.Next(1, 11);
+                int tempUserIndex;
+                int score;
+                if (userList.Contains(name))
                 {
-                    int tempUserIndex = userList.IndexOf(user);
-                    string userBack = string.Format("Du {0} har {1} poäng!", user, tempUserScore);
-                    Console.WriteLine(userBack);
-                    int score = 1;
-                    userScore[tempUserIndex] += score;
+                    tempUserIndex = userList.FindIndex(a => a.Contains(name));
+                    score = userScore[tempUserIndex];
                 }
+                else 
+                {
+                    userList.Add(name);
+                    userScore.Add(0);
+                    tempUserIndex = userList.Count();
+                    score = userScore[tempUserIndex];
+                }
+                string userBack = string.Format("Du {0} har {1} poäng!", userList[tempUserIndex], score);
+                Console.SetCursorPosition(windowWidth.SetWidth(userBack), windowWidth.SetXpos(5));
+                Console.Write(userBack);
+                Console.SetCursorPosition(windowWidth.MaxWidth(), windowWidth.SetXpos(1));
+                Console.Write("                                                                        ");
+                string gissaText = "Gissa ett nummer mellan 1 - 10";
+                Console.SetCursorPosition(windowWidth.SetWidth(gissaText), windowWidth.SetXpos());
+                Console.Write("                                              ");
+                Console.SetCursorPosition(windowWidth.SetWidth(gissaText), windowWidth.SetXpos());
+                Console.Write(gissaText);
+                Console.SetCursorPosition(windowWidth.MaxWidth(), windowWidth.SetXpos(1));
+                Console.Write("> ");
+                int gissning = 0;
+                string aGissning = Console.ReadLine();
+                try 
+                {
+                    gissning = Int32.Parse(aGissning);
+                }
+                catch (FormatException) 
+                {
+                    string error = "Du måste skriva in ett nummer!";
+                    Console.SetCursorPosition(windowWidth.SetWidth(error), windowWidth.SetXpos(3));
+                    Console.Write(error);
+                }
+                if (gissning == slumpTal)
+                {
+                    string correct = "Du gissade rätt!";
+                    string press = "Tryck på (N) för att avsluta eller, Tryck på valfri tangent för att fortsätta.";
+                    Console.SetCursorPosition(windowWidth.SetWidth(gissaText), windowWidth.SetXpos(-1));
+                    Console.Write("                                                                                    ");
+                    Console.SetCursorPosition(windowWidth.SetWidth(correct), windowWidth.SetXpos(-1));
+                    Console.Write(correct);
+                    Console.SetCursorPosition(windowWidth.SetWidth(press), windowWidth.SetXpos());
+                    Console.Write(press);
+                    Console.SetCursorPosition(windowWidth.MaxWidth(), windowWidth.SetXpos(1));
+                    Console.Write("> ");
+                    string yN = Console.ReadLine().ToLower();
+                    ++score;
+                    userScore.Insert(tempUserIndex, score);
+                    Console.SetCursorPosition(windowWidth.SetWidth(press), windowWidth.SetXpos());
+                    Console.Write("                                                                                 ");
+                    if (yN == "n") nyttSpel = false;
+                    else 
+                    {
+                        Console.SetCursorPosition(windowWidth.SetWidth(correct), windowWidth.SetXpos(-1));
+                        Console.Write("                                                                                    ");
+                        Console.SetCursorPosition(windowWidth.SetWidth(correct), windowWidth.SetXpos());
+                        Console.Write("                                                                                    ");
+                    }
+                }
+
+                else if (gissning < slumpTal)
+                {   
+                    string guessLow = "Du gissade lägre än talet.";
+                    Console.SetCursorPosition((windowWidth.MaxWidth()), Xpos-1);
+                    Console.Write("                                                                                    ");
+                    Console.SetCursorPosition(windowWidth.SetWidth(guessLow), windowWidth.SetXpos(-1));
+                    Console.Write(guessLow);
+                }
+
+                else
+                {
+                    string guessHigh = "Du gissade högre än talet.";
+                    Console.SetCursorPosition(windowWidth.MaxWidth(), windowWidth.SetXpos(-1));
+                    Console.Write("                                                                                    ");
+                    Console.SetCursorPosition(windowWidth.SetWidth(guessHigh), windowWidth.SetXpos(-1));
+                    Console.WriteLine(guessHigh);
+                }
+
             }
 
-            for (int i = 0; i < userScore.Count; i++)
-            {
-            }
+
+            Console.WriteLine();
         }
         public static void Highscore()
         {
-            
+            string title = @"
+                        ██░ ██  ██▓  ▄████  ██░ ██   ██████  ▄████▄   ▒█████   ██▀███  ▓█████  ▐██▌ 
+                       ▓██░ ██▒▓██▒ ██▒ ▀█▒▓██░ ██▒▒██    ▒ ▒██▀ ▀█  ▒██▒  ██▒▓██ ▒ ██▒▓█   ▀  ▐██▌ 
+                       ▒██▀▀██░▒██▒▒██░▄▄▄░▒██▀▀██░░ ▓██▄   ▒▓█    ▄ ▒██░  ██▒▓██ ░▄█ ▒▒███    ▐██▌ 
+                       ░▓█ ░██ ░██░░▓█  ██▓░▓█ ░██   ▒   ██▒▒▓▓▄ ▄██▒▒██   ██░▒██▀▀█▄  ▒▓█  ▄  ▓██▒ 
+                       ░▓█▒░██▓░██░░▒▓███▀▒░▓█▒░██▓▒██████▒▒▒ ▓███▀ ░░ ████▓▒░░██▓ ▒██▒░▒████▒ ▒▄▄  
+                        ▒ ░░▒░▒░▓   ░▒   ▒  ▒ ░░▒░▒▒ ▒▓▒ ▒ ░░ ░▒ ▒  ░░ ▒░▒░▒░ ░ ▒▓ ░▒▓░░░ ▒░ ░ ░▀▀▒ 
+                        ▒ ░▒░ ░ ▒ ░  ░   ░  ▒ ░▒░ ░░ ░▒  ░ ░  ░  ▒     ░ ▒ ▒░   ░▒ ░ ▒░ ░ ░  ░ ░  ░ 
+                        ░  ░░ ░ ▒ ░░ ░   ░  ░  ░░ ░░  ░  ░  ░        ░ ░ ░ ▒    ░░   ░    ░       ░ 
+                        ░  ░  ░ ░        ░  ░  ░  ░      ░  ░ ░          ░ ░     ░        ░  ░ ░    
+                                            ░                                                      ";
+            Console.ForegroundColor = ConsoleColor.DarkRed; Console.WriteLine(title);
+            Console.ForegroundColor = ConsoleColor.White;
+            List<string> highScore = new List<string>();
+            if (userList.Count == 0) 
+            {
+                userList.Add("Local Extremum");
+                userScore.Add(3);
+                userList.Add("The Double Equation");
+                userScore.Add(1);
+                userList.Add("Root Of Pi");
+                userScore.Add(4);
+                userList.Add("Golden ratio");
+                userScore.Add(2);
+            }
+            foreach (var user in userList)
+            {
+                int tempIndex = userList.IndexOf(user);
+                highScore.Add(userScore[tempIndex].ToString() + " | " + user);
+            }
+            highScore.Sort();
+            highScore.Reverse();
+            string description = "POÄNG | NAMN";
+            Console.SetCursorPosition(windowWidth.SetWidth(description), windowWidth.SetXpos());
+            Console.Write(description);
+            foreach (var user in highScore) 
+            {
+                ++Xpos;
+                Console.SetCursorPosition(windowWidth.SetWidth(user), ++Xpos);
+                Console.Write(user);
+            }
+            string pressAny = "Tryck på valfri knapp för att återgå till huvudmenyn.";
+            Console.SetCursorPosition(windowWidth.SetWidth(pressAny), windowWidth.SetXpos(5));
+            Console.Write(pressAny);
+            Console.ReadLine();
         }
 
         public static void Titel()
         {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkRed;
             string titel = @"
-                  ▄████  ██▓  ██████   ██████  ▄▄▄          ▒█████   ██▀███  ▓█████▄ ▓█████▄▄▄█████▓
-                 ██▒ ▀█▒▓██▒▒██    ▒ ▒██    ▒ ▒████▄       ▒██▒  ██▒▓██ ▒ ██▒▒██▀ ██▌▓█   ▀▓  ██▒ ▓▒
-                ▒██░▄▄▄░▒██▒░ ▓██▄   ░ ▓██▄   ▒██  ▀█▄     ▒██░  ██▒▓██ ░▄█ ▒░██   █▌▒███  ▒ ▓██░ ▒░
-                ░▓█  ██▓░██░  ▒   ██▒  ▒   ██▒░██▄▄▄▄██    ▒██   ██░▒██▀▀█▄  ░▓█▄   ▌▒▓█  ▄░ ▓██▓ ░ 
-                ░▒▓███▀▒░██░▒██████▒▒▒██████▒▒ ▓█   ▓██▒   ░ ████▓▒░░██▓ ▒██▒░▒████▓ ░▒████▒ ▒██▒ ░ 
-                    ░▒   ▒ ░▓  ▒ ▒▓▒ ▒ ░▒ ▒▓▒ ▒ ░ ▒▒   ▓▒█░   ░ ▒░▒░▒░ ░ ▒▓ ░▒▓░ ▒▒▓  ▒ ░░ ▒░ ░ ▒ ░░   
-                    ░   ░  ▒ ░░ ░▒  ░ ░░ ░▒  ░ ░  ▒   ▒▒ ░     ░ ▒ ▒░   ░▒ ░ ▒░ ░ ▒  ▒  ░ ░  ░   ░    
-                ░ ░   ░  ▒ ░░  ░  ░  ░  ░  ░    ░   ▒      ░ ░ ░ ▒    ░░   ░  ░ ░  ░    ░    ░      
-                        ░  ░        ░        ░        ░  ░       ░ ░     ░        ░       ░  ░        
-                                                                                ░                     ";
+                  ▄████  ██▓  ██████   ██████  ▄▄▄         ▄▄▄█████▓ ▄▄▄       ██▓    ▓█████▄▄▄█████▓
+                 ██▒ ▀█▒▓██▒▒██    ▒ ▒██    ▒ ▒████▄       ▓  ██▒ ▓▒▒████▄    ▓██▒    ▓█   ▀▓  ██▒ ▓▒
+                ▒██░▄▄▄░▒██▒░ ▓██▄   ░ ▓██▄   ▒██  ▀█▄     ▒ ▓██░ ▒░▒██  ▀█▄  ▒██░    ▒███  ▒ ▓██░ ▒░
+                ░▓█  ██▓░██░  ▒   ██▒  ▒   ██▒░██▄▄▄▄██    ░ ▓██▓ ░ ░██▄▄▄▄██ ▒██░    ▒▓█  ▄░ ▓██▓ ░ 
+                ░▒▓███▀▒░██░▒██████▒▒▒██████▒▒ ▓█   ▓██▒     ▒██▒ ░  ▓█   ▓██▒░██████▒░▒████▒ ▒██▒ ░ 
+                 ░▒   ▒ ░▓  ▒ ▒▓▒ ▒ ░▒ ▒▓▒ ▒ ░ ▒▒   ▓▒█░     ▒ ░░    ▒▒   ▓▒█░░ ▒░▓  ░░░ ▒░ ░ ▒ ░░   
+                  ░   ░  ▒ ░░ ░▒  ░ ░░ ░▒  ░ ░  ▒   ▒▒ ░       ░      ▒   ▒▒ ░░ ░ ▒  ░ ░ ░  ░   ░    
+                ░ ░   ░  ▒ ░░  ░  ░  ░  ░  ░    ░   ▒        ░        ░   ▒     ░ ░      ░    ░      
+                      ░  ░        ░        ░        ░  ░                  ░  ░    ░  ░   ░  ░        
+                                                                                     ";
             Console.WriteLine(titel);
 
+        }
+        public class WindowWidth
+        {
+            public int MaxWidth() 
+            {
+                return Console.WindowWidth/2;
+            }
+            public int SetWidth(string word) 
+            {
+                return MaxWidth() -( word.Length/2 );
+            }
+            public int SetXpos() 
+            {
+                return Xpos;
+            }
+            public int SetXpos(int yneg) 
+            {
+                return Xpos + (yneg);
+            }
         }
     }
 }
